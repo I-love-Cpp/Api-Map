3445af30d121505cdda2dde0dd5964d46ea79007
Andrey <andrey.a.degterev@gmail.com>
2021-02-08 18:55:53 +0300

    second version

--- map.py
+++ map.py
@@ -1,12 +1,8 @@
 import os
-import sys
 
 import pygame
-
-from find_spn import find_s
-
 import requests
-from PIL import Image
+from find_spn import find_s
 
 x, y = map(float, input().split())
 
@@ -31,26 +27,38 @@ ll, spn = find_s(json_response)
 
 point = ll.split(',')
 
-org_point = "{0},{1}".format(point[0], point[1])
-map_params = {
-    "ll": ll,
-    "spn": spn,
-    "l": "map",
-    "pt": "{0},pm2dgl".format(org_point)
-}
 
-map_api_server = "http://static-maps.yandex.ru/1.x/"
-response = requests.get(map_api_server, params=map_params)
+def load_map(spn):
+    org_point = "{0},{1}".format(point[0], point[1])
+    map_params = {
+        "ll": ll,
+        "spn": spn,
+        "l": "map",
+        "pt": "{0},pm2dgl".format(org_point)
+    }
 
-map_file = "map.png"
-with open(map_file, "wb") as file:
-    file.write(response.content)
+    map_api_server = "http://static-maps.yandex.ru/1.x/"
+    response = requests.get(map_api_server, params=map_params)
+
+    map_file = "map.png"
+    with open(map_file, "wb") as file:
+        file.write(response.content)
+    return map_file
+
+
+map_file = load_map(spn)
 
 pygame.init()
 screen = pygame.display.set_mode((600, 450))
 screen.blit(pygame.image.load(map_file), (0, 0))
 pygame.display.flip()
 while pygame.event.wait().type != pygame.QUIT:
-    pass
+    for event in pygame.event.get():
+        if pygame.key.get_pressed()[pygame.K_PAGEUP]:
+            spn = f"{int(spn.split(',')[0]) - 0.1},{int(spn.split(',')[1]) - 0.1}"
+            load_map(spn)
+        elif pygame.key.get_pressed()[pygame.K_PAGEDOWN]:
+            spn = f"{int(spn.split(',')[0]) + 0.1},{int(spn.split(',')[1]) + 0.1}"
+            load_map(spn)
 pygame.quit()
 os.remove(map_file)
